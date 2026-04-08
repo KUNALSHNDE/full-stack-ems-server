33b9e0dca1cc559f81cd01f42df47ba708840de6
KUNALSHNDE <shindekunal267@gmail.com>
2026-04-08 11:42:17 +0530

    Cors error solved

--- app.js
+++ app.js
@@ -14,7 +14,18 @@ import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://employee-management-system-ivis0x7z-kunalshinde-projects.vercel.app",
+      "http://localhost:3000",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 app.use(multer().none());
 
